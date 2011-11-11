959579e6ea1b8f728ae09f85715b389c6c0d090a
daifu ye <daifu.ye@gmail.com>
2011-11-11 02:59:48 -0800

    UI Updated
    Updated ui for course details page. And minify app.'s and style.css.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,15 +1,4 @@
-addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);function hideURLbar(){window.scrollTo(0,1);}
 
-function hideURLbar(){
-  window.scrollTo(0,1);
-}
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-26836091-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-26836091-1']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
