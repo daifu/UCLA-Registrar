51e3c914e2194955b13935356e62f96744857839
daifu ye <daifu.ye@gmail.com>
2011-12-26 01:25:07 -0800

    Changed default host and port by using variables

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ var express = require('express')
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+   app.set('views', __dirname + '/views');
+   app.set('view engine', 'jade');
+   app.use(express.bodyParser());
+   app.use(express.methodOverride());
+   app.use(app.router);
+   app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
