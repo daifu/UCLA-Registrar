b89c1655498fdb108eae5e0e6d68dd24a0f5cd67
daifu <daifu.ye@gmail.com>
2012-11-23 00:09:17 -0800

    Fixed the transition

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,2 +1,3 @@
 var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-26836091-1']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+$(document).bind("mobileinit",function(){$.mobile.defaultPageTransition="slide";});
